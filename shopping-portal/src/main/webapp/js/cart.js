b0edbe826dfbac6a37395b0a7b1b66b948ec5ea0
qinf <908247035@qq.com>
2016-12-09 16:20:20 +0800

    分支合并

--- shopping-portal/src/main/webapp/js/cart.js
+++ shopping-portal/src/main/webapp/js/cart.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var TTCart = {
 	load : function(){ // 加载购物车数据
 		
@@ -46,53 +45,4 @@ var TTCart = {
 $(function(){
 	TTCart.load();
 	TTCart.itemNumChange();
-=======
-var TTCart = {
-	load : function(){ // 加载购物车数据
-		
-	},
-	itemNumChange : function(){
-		$(".increment").click(function(){//＋
-			var _thisInput = $(this).siblings("input");
-			_thisInput.val(eval(_thisInput.val()) + 1);
-			$.post("/cart/update/num/"+_thisInput.attr("itemId")+"/"+_thisInput.val() + ".action",function(data){
-				TTCart.refreshTotalPrice();
-			});
-		});
-		$(".decrement").click(function(){//-
-			var _thisInput = $(this).siblings("input");
-			if(eval(_thisInput.val()) == 1){
-				return ;
-			}
-			_thisInput.val(eval(_thisInput.val()) - 1);
-			$.post("/cart/update/num/"+_thisInput.attr("itemId")+"/"+_thisInput.val() + ".action",function(data){
-				TTCart.refreshTotalPrice();
-			});
-		});
-		$(".quantity-form .quantity-text").rnumber(1);//限制只能输入数字
-		$(".quantity-form .quantity-text").change(function(){
-			var _thisInput = $(this);
-			$.post("/service/cart/update/num/"+_thisInput.attr("itemId")+"/"+_thisInput.val(),function(data){
-				TTCart.refreshTotalPrice();
-			});
-		});
-	},
-	refreshTotalPrice : function(){ //重新计算总价
-		var total = 0;
-		$(".quantity-form .quantity-text").each(function(i,e){
-			var _this = $(e);
-			total += (eval(_this.attr("itemPrice")) * 10000 * eval(_this.val())) / 10000;
-		});
-		$(".totalSkuPrice").html(new Number(total/100).toFixed(2)).priceFormat({ //价格格式化插件
-			 prefix: '￥',
-			 thousandsSeparator: ',',
-			 centsLimit: 2
-		});
-	}
-};
-
-$(function(){
-	TTCart.load();
-	TTCart.itemNumChange();
->>>>>>> bf005f330c461f7684e15b86f0bb330bccc47e5f
 });
